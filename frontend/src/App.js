c918c7862482a5afc348df928fce8d704644c76d
djgnfj-svg <djgnfj8923@naver.com>
2023-01-20 01:11:18 +0900

    add page diet_meal_list

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,8 +3,9 @@ import './temp.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import MyNavbar from './components/navbar';
 import Input_form from './pages/body_info_input/input_form/input_form';
-import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
 import Output_form from './pages/body_info_output/body_info_output';
+import { Routes, Route, Link, useNavigate, Outlet } from 'react-router-dom'
+import Diet_meal_list from './pages/diet_meal_list/diet_meal_list';
 function App() {
   return (
     <div className="app">
@@ -12,7 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Input_form />} />
         <Route path="*" element={ <div>없는페이지임</div> } />
-        <Route path="output" element={<Output_form />}>
+        <Route path="output" element={<Output_form />} />
+        <Route path="diet_meal_list" element={<Diet_meal_list />}>
 
         </Route>
       </Routes>
---
+++ frontend/src/pages/diet_meal_list/diet_meal_list.js
@@ -0,0 +1,235 @@
+import "./diet_meal_list.css"
+
+
+function Diet_meal_list() {
+    return (
+        <>
+            <div className="flip-body">
+                {/* flip-card-container */}
+                <div className="flip-card-container" style={{ "-hue": 220 }}>
+                    <div className="flip-card">
+                        <div className="card-front">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1486162928267-e6274cb3106f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Brohm Lake" />
+                                <figcaption>Brohm Lake</figcaption>
+                            </figure>
+                            <ul>
+                                <li>Detail 1</li>
+                                <li>Detail 2</li>
+                                <li>Detail 3</li>
+                                <li>Detail 4</li>
+                                <li>Detail 5</li>
+                            </ul>
+                        </div>
+                        <div className="card-back">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1486162928267-e6274cb3106f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Brohm Lake" />
+                            </figure>
+                            <button>Book</button>
+                            <div className="design-container">
+                                <span className="design design--4" />
+                                <span className="design design--5" />
+                                <span className="design design--6" />
+                                <span className="design design--7" />
+                                <span className="design design--8" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* /flip-card-container */}
+                {/* flip-card-container */}
+                <div className="flip-card-container" style={{ "-hue": 170 }}>
+                    <div className="flip-card">
+                        <div className="card-front">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Image 2" />
+                                <figcaption>Légende</figcaption>
+                            </figure>
+                            <ul>
+                                <li>Detail 1</li>
+                                <li>Detail 2</li>
+                                <li>Detail 3</li>
+                                <li>Detail 4</li>
+                                <li>Detail 5</li>
+                            </ul>
+                        </div>
+                        <div className="card-back">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="image-2" />
+                            </figure>
+                            <button>Book</button>
+                            <div className="design-container">
+                                <span className="design design--1" />
+                                <span className="design design--2" />
+                                <span className="design design--3" />
+                                <span className="design design--4" />
+                                <span className="design design--5" />
+                                <span className="design design--6" />
+                                <span className="design design--7" />
+                                <span className="design design--8" />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                {/* /flip-card-container */}
+                {/* flip-card-container */}
+                <div className="flip-card-container" style={{ "-hue": 170 }}>
+                    <div className="flip-card">
+                        <div className="card-front">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Image 2" />
+                                <figcaption>Légende</figcaption>
+                            </figure>
+                            <ul>
+                                <li>Detail 1</li>
+                                <li>Detail 2</li>
+                                <li>Detail 3</li>
+                                <li>Detail 4</li>
+                                <li>Detail 5</li>
+                            </ul>
+                        </div>
+                        <div className="card-back">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="image-2" />
+                            </figure>
+                            <button>Book</button>
+                            <div className="design-container">
+                                <span className="design design--1" />
+                                <span className="design design--2" />
+                                <span className="design design--3" />
+                                <span className="design design--4" />
+                                <span className="design design--5" />
+                                <span className="design design--6" />
+                                <span className="design design--7" />
+                                <span className="design design--8" />
+                            </div>
+                        </div>
+                    </div>
+                </div >
+                {/* /flip-card-container */}
+                {/* flip-card-container */}
+                <div className="flip-card-container" style={{
+                    "-hue": 170
+                }}>
+                    <div className="flip-card">
+                        <div className="card-front">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Image 2" />
+                                <figcaption>Légende</figcaption>
+                            </figure>
+                            <ul>
+                                <li>Detail 1</li>
+                                <li>Detail 2</li>
+                                <li>Detail 3</li>
+                                <li>Detail 4</li>
+                                <li>Detail 5</li>
+                            </ul>
+                        </div>
+                        <div className="card-back">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="image-2" />
+                            </figure>
+                            <button>Book</button>
+                            <div className="design-container">
+                                <span className="design design--1" />
+                                <span className="design design--2" />
+                                <span className="design design--3" />
+                                <span className="design design--4" />
+                                <span className="design design--5" />
+                                <span className="design design--6" />
+                                <span className="design design--7" />
+                                <span className="design design--8" />
+                            </div>
+                        </div>
+                    </div>
+                </div >
+                {/* /flip-card-container */}
+                {/* flip-card-container */}
+                <div className="flip-card-container" style={{ "-hue": 170 }}>
+                    <div className="flip-card">
+                        <div className="card-front">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Image 2" />
+                                <figcaption>Légende</figcaption>
+                            </figure>
+                            <ul>
+                                <li>Detail 1</li>
+                                <li>Detail 2</li>
+                                <li>Detail 3</li>
+                                <li>Detail 4</li>
+                                <li>Detail 5</li>
+                            </ul>
+                        </div>
+                        <div className="card-back">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="image-2" />
+                            </figure>
+                            <button>Book</button>
+                            <div className="design-container">
+                                <span className="design design--1" />
+                                <span className="design design--2" />
+                                <span className="design design--3" />
+                                <span className="design design--4" />
+                                <span className="design design--5" />
+                                <span className="design design--6" />
+                                <span className="design design--7" />
+                                <span className="design design--8" />
+                            </div>
+                        </div>
+                    </div>
+                </div >
+                {/* /flip-card-container */}
+                {/* flip-card-container */}
+                <div className="flip-card-container" style={{ "-hue": 350 }}>
+                    <div className="flip-card">
+                        <div className="card-front">
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1486162928267-e6274cb3106f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Brohm Lake" />
+                                <figcaption>Brohm Lake</figcaption>
+                            </figure>
+                            <ul>
+                                <li>Detail 1</li>
+                                <li>Detail 2</li>
+                                <li>Detail 3</li>
+                                <li>Detail 4</li>
+                                <li>Detail 5</li>
+                            </ul>
+                        </div>
+                        <div className="card-back">
+                            {/* only if the image is necessary */}
+                            <figure>
+                                <div className="img-bg" />
+                                <img src="https://images.unsplash.com/photo-1486162928267-e6274cb3106f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Brohm Lake" />
+                            </figure>
+                            <button>Book</button>
+                            {/* can add svg here and remove these eight spans */}
+                            <div className="design-container">
+                                <span className="design design--1" />
+                                <span className="design design--2" />
+                                <span className="design design--3" />
+                                <span className="design design--4" />
+                                <span className="design design--5" />
+                                <span className="design design--6" />
+                                <span className="design design--7" />
+                                <span className="design design--8" />
+                            </div>
+                        </div>
+                    </div>
+                </div >
+            </div>
+        </>
+    )
+}
+
+export default Diet_meal_list;
