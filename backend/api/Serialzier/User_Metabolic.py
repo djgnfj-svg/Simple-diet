c3f5b27bd906b42c9ef7efec032b7e6a93e0e724
djgnfj-svg <djgnfj8923@naver.com>
2023-01-23 00:10:29 +0900

    finish food master data

--- backend/api/Serialzier/User_Metabolic.py
+++ backend/api/Serialzier/User_Metabolic.py
@@ -35,48 +35,19 @@ class User_body_info_SZ(serializers.Serializer):
         3. 주간데이터를 통해서 먹어야하는 주간음식데이터를 만든다
             음식데이터 모델을 만든다. o
             음식을 추가하는 API를 만든다.o
-            음식을 30개 정도 추가한다 <- ㅈㄴ힘든데? ㅋㅋㅋㅋ
+            음식을 추가할때 검증해야 하는 부분을 만든다 (링크중에 마지막넘버가 같으게 있으면 업데이트 창으로 간다거나)
                 ex)
                 주간 단백질 1000g
                 우선순위에 따라서 측정하자 결국 DB Food 테이블에 우선순위는 존재해야한다.
                 아니면 내가 그냥 가격순으로 정렬해 버리든가.
-                1. 닭가슴살
-                    https://www.coupang.com/vp/products/6081081115?itemId=11273250763
-                    https://www.coupang.com/vp/products/1519330646?itemId=5810607566
-                    https://www.coupang.com/vp/products/4656203901?itemId=5810700340
-                2. 야채...?
-                    토마토
-                    아보카도
-                3. 냉동식품 - 육류
-                    https://www.coupang.com/vp/products/1271810164?itemId=2276602192
-                    https://www.coupang.com/vp/products/1608824364?itemId=2747730175
-                    https://www.coupang.com/vp/products/4567751809?itemId=5575412259
-                    https://www.coupang.com/vp/products/1583514552?itemId=2706863599
-                4. 즉석밥
-                    https://www.coupang.com/vp/products/1391921882?itemId=2427315006
-                    https://www.coupang.com/vp/products/78749832?itemId=550725955
-                5. 그냥 소세지
-                    https://www.coupang.com/vp/products/346980667?itemId=1101451330
-                    https://www.coupang.com/vp/products/2124428252?itemId=3604872129
-                    https://www.coupang.com/vp/products/1756604528?itemId=2991836703
-                6. 닭가슴살 스테이크
-                    https://www.coupang.com/vp/products/2171896373?itemId=3694584054
-                    https://www.coupang.com/vp/products/4926123653?itemId=6464747257
-                    https://www.coupang.com/vp/products/343254008?itemId=1090857293
-                7. 닭가슴살 소세지
-                    https://www.coupang.com/vp/products/1915681699?itemId=3252475357
-                8. 냉동 볶음밥
-                    https://www.coupang.com/vp/products/185917324?itemId=983874261
-                    https://www.coupang.com/vp/products/1310396687?itemId=2327020830
-                    https://www.coupang.com/vp/products/343228726?itemId=1090788751
-                    https://www.coupang.com/vp/products/6072624005?itemId=11218284903
-                9. 크래미
-                    https://www.coupang.com/vp/products/280767413?itemId=891150359
-                    https://www.coupang.com/vp/products/280767362?itemId=891150262
-                10. 다이어트 도시락
-                    https://www.coupang.com/vp/products/293454508?itemId=926514679
-                    https://www.coupang.com/vp/products/6373068955?itemId=13507557295
-                # 나중에 커스텀으로 식이섬유를 챙기시겠습니가?
+
+                여러개의 종목이 있는 품목에 대하여
+                - 각각 더해서 평균을 정하는게 편할 듯 하다.
+                그래
+                - 주먹밥
+                - 핫도그
+                추가하자
+               # 나중에 커스텀으로 식이섬유를 챙기시겠습니가?
                 # 장점
                 # 똥 굳
                 # 단점
