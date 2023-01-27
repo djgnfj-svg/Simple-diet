768dd90589a7a662708879193c272cd0186e536c
djgnfj-svg <djgnfj8923@naver.com>
2023-01-27 18:23:50 +0900

    add front axios diet_managimant

--- frontend/src/pages/body_info_output/body_info_output.js
+++ frontend/src/pages/body_info_output/body_info_output.js
@@ -1,88 +1,83 @@
 import { React, useState } from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
 import { useLocation, useNavigate } from 'react-router';
+import axios from "axios";
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './body_info_output.css';
 
 function Output_form() {
-    const { state } = useLocation();
-    return (
-        <section className="section-plans" id="section-plans">
-        <div className="u-center-text u-margin-bottom-big">
-          <h2 className="heading-secondary">
-            끼니 정보
-          </h2>
-        </div>
-        <div className="row">
-          <div className="col-1-of-3">
-            <div className="card">
-              <div className="card__side card__side--front-1">
-                <div className="card__title card__title--1">
-                  <i className="fas fa-paper-plane" />
-                  <h4 className="card__heading">그림?</h4>
-                </div>
-                <div className="card__details">
-                  <ul>
-                    <li>아침</li>
-                    <li>칼로리 : {state.meals["1_meals"].kilo_calorie}kcal</li>
-                    <li>탄수화물 : {state.meals["1_meals"].carbohydrate}g</li>
-                    <li>단백질 : {state.meals["1_meals"].protein}g</li>
-                    <li>지방 : {state.meals["1_meals"].fat}g</li>
-                  </ul>
-                </div>
+  const navigate = useNavigate();
+  const { state } = useLocation();
+  const _data = {data:state}
+
+  const navigateToOutput = () => {
+    axios.post("http://127.0.0.1:8000/api/diet_diet/", _data)
+      .then((e) => {
+        navigate('/diet_meal_list', { data: e.data });
+      })
+      .catch(() => {
+        alert("실패")
+      })
+  }
+  return (
+    <section className="section-plans" id="section-plans">
+      <div className="u-center-text u-margin-bottom-big">
+        <h2 className="heading-secondary">
+          끼니 정보
+        </h2>
+      </div>
+      <div className="row">
+        <div className="col-1-of-3">
+          <div className="card">
+            <div className="card__side card__side--front-1">
+              <div className="card__details">
+                <ul>
+                  <li>아침</li>
+                  <li>칼로리 : {state.breakfast.kilo_calorie}kcal</li>
+                  <li>탄수화물 : {state.breakfast.carbohydrate}g</li>
+                  <li>단백질 : {state.breakfast.protein}g</li>
+                  <li>지방 : {state.breakfast.fat}g</li>
+                </ul>
               </div>
             </div>
           </div>
-          <div className="col-1-of-3">
-            <div className="card">
-              <div className="card__side card__side--front-2">
-                <div className="card__title card__title--2">
-                  <i className="fas fa-plane" />
-                  <h4 className="card__heading">그림</h4>
-                </div>
-                <div className="card__details">
-                  <ul>
+        </div>
+        <div className="col-1-of-3">
+          <div className="card">
+            <div className="card__side card__side--front-2">
+              <div className="card__details">
+                <ul>
                   <li>점심</li>
-                  <li>칼로리 : {state.meals["2_meals"].kilo_calorie}kcal</li>
-                    <li>탄수화물 : {state.meals["2_meals"].carbohydrate}g</li>
-                    <li>단백질 : {state.meals["2_meals"].protein}g</li>
-                    <li>지방 : {state.meals["2_meals"].fat}g</li>
-                  </ul>
-                </div>
+                  <li>칼로리 : {state.lunch.kilo_calorie}kcal</li>
+                  <li>탄수화물 : {state.lunch.carbohydrate}g</li>
+                  <li>단백질 : {state.lunch.protein}g</li>
+                  <li>지방 : {state.lunch.fat}g</li>
+                </ul>
               </div>
             </div>
           </div>
-          {
-            Object.keys(state.meals).length==3
-            ?           
-            <div className="col-1-of-3">
-            <div className="card">
-              <div className="card__side card__side--front-3">
-                <div className="card__title card__title--3">
-                  <i className="fas fa-rocket" />
-                  <h4 className="card__heading">그림</h4>
-                </div>
-                <div className="card__details">
-                  <ul>
+        </div>
+        <div className="col-1-of-3">
+          <div className="card">
+            <div className="card__side card__side--front-3">
+              <div className="card__details">
+                <ul>
                   <li>저녁</li>
-                  <li>칼로리 : {state.meals["3_meals"].kilo_calorie}kcal</li>
-                    <li>탄수화물 : {state.meals["3_meals"].carbohydrate}g</li>
-                    <li>단백질 : {state.meals["3_meals"].protein}g</li>
-                    <li>지방 : {state.meals["3_meals"].fat}g</li>
-                  </ul>
-                </div>
+                  <li>칼로리 : {state.dinner.kilo_calorie}kcal</li>
+                  <li>탄수화물 : {state.dinner.carbohydrate}g</li>
+                  <li>단백질 : {state.dinner.protein}g</li>
+                  <li>지방 : {state.dinner.fat}g</li>
+                </ul>
               </div>
             </div>
           </div>
-          :
-          null
-          }
         </div>
-        <div className="u-center-text u-margin-top-huge">
-          {/* todo 추후 쿠팡 식단 계산으로 이동 */}
-          <a href="/" className="btn btn--green">Get Started</a>
-        </div>
-      </section>
-    )
+      </div>
+      <div className="u-center-text u-margin-top-huge">
+        <a onClick={navigateToOutput} className="btn btn--green">쿠팡 식단 계산</a>
+        {/* todo 추후 쿠팡 식단 계산으로 이동 */}
+      </div>
+    </section>
+  )
 }
 
 export default Output_form;
--- frontend/src/pages/diet_meal_list/diet_meal_list.js
+++ frontend/src/pages/diet_meal_list/diet_meal_list.js
@@ -11,7 +11,7 @@ function Diet_meal_list() {
                         <div className="card-front">
                             <figure>
                                 <div className="img-bg" />
-                                <img src="https://images.unsplash.com/photo-1486162928267-e6274cb3106f?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Brohm Lake" />
+                                <img src="https://cdn.kormedi.com/wp-content/uploads/2021/08/gettyimages-1194703474-580x439.jpg" alt="Brohm Lake" />
                                 <figcaption>Brohm Lake</figcaption>
                             </figure>
                             <ul>
@@ -38,119 +38,6 @@ function Diet_meal_list() {
                         </div>
                     </div>
                 </div>
-                {/* /flip-card-container */}
-                {/* flip-card-container */}
-                <div className="flip-card-container" style={{ "-hue": 170 }}>
-                    <div className="flip-card">
-                        <div className="card-front">
-                            <figure>
-                                <div className="img-bg" />
-                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Image 2" />
-                                <figcaption>Légende</figcaption>
-                            </figure>
-                            <ul>
-                                <li>Detail 1</li>
-                                <li>Detail 2</li>
-                                <li>Detail 3</li>
-                                <li>Detail 4</li>
-                                <li>Detail 5</li>
-                            </ul>
-                        </div>
-                        <div className="card-back">
-                            <figure>
-                                <div className="img-bg" />
-                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="image-2" />
-                            </figure>
-                            <button>Book</button>
-                            <div className="design-container">
-                                <span className="design design--1" />
-                                <span className="design design--2" />
-                                <span className="design design--3" />
-                                <span className="design design--4" />
-                                <span className="design design--5" />
-                                <span className="design design--6" />
-                                <span className="design design--7" />
-                                <span className="design design--8" />
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                {/* /flip-card-container */}
-                {/* flip-card-container */}
-                <div className="flip-card-container" style={{ "-hue": 170 }}>
-                    <div className="flip-card">
-                        <div className="card-front">
-                            <figure>
-                                <div className="img-bg" />
-                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Image 2" />
-                                <figcaption>Légende</figcaption>
-                            </figure>
-                            <ul>
-                                <li>Detail 1</li>
-                                <li>Detail 2</li>
-                                <li>Detail 3</li>
-                                <li>Detail 4</li>
-                                <li>Detail 5</li>
-                            </ul>
-                        </div>
-                        <div className="card-back">
-                            <figure>
-                                <div className="img-bg" />
-                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="image-2" />
-                            </figure>
-                            <button>Book</button>
-                            <div className="design-container">
-                                <span className="design design--1" />
-                                <span className="design design--2" />
-                                <span className="design design--3" />
-                                <span className="design design--4" />
-                                <span className="design design--5" />
-                                <span className="design design--6" />
-                                <span className="design design--7" />
-                                <span className="design design--8" />
-                            </div>
-                        </div>
-                    </div>
-                </div >
-                {/* /flip-card-container */}
-                {/* flip-card-container */}
-                <div className="flip-card-container" style={{
-                    "-hue": 170
-                }}>
-                    <div className="flip-card">
-                        <div className="card-front">
-                            <figure>
-                                <div className="img-bg" />
-                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="Image 2" />
-                                <figcaption>Légende</figcaption>
-                            </figure>
-                            <ul>
-                                <li>Detail 1</li>
-                                <li>Detail 2</li>
-                                <li>Detail 3</li>
-                                <li>Detail 4</li>
-                                <li>Detail 5</li>
-                            </ul>
-                        </div>
-                        <div className="card-back">
-                            <figure>
-                                <div className="img-bg" />
-                                <img src="https://images.unsplash.com/photo-1545436864-cd9bdd1ddebc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60" alt="image-2" />
-                            </figure>
-                            <button>Book</button>
-                            <div className="design-container">
-                                <span className="design design--1" />
-                                <span className="design design--2" />
-                                <span className="design design--3" />
-                                <span className="design design--4" />
-                                <span className="design design--5" />
-                                <span className="design design--6" />
-                                <span className="design design--7" />
-                                <span className="design design--8" />
-                            </div>
-                        </div>
-                    </div>
-                </div >
                 {/* /flip-card-container */}
                 {/* flip-card-container */}
                 <div className="flip-card-container" style={{ "-hue": 170 }}>
