5e9bad50a4a1679df01b2aa2ee92f18f8eaec2d5
djgnfj-svg <djgnfj8923@naver.com>
2023-02-28 03:28:23 +0900

    add metabbloci view

---
+++ backend/api/Views/Metabolic_Calc_Viewset.py
@@ -0,0 +1,17 @@
+from rest_framework import status, viewsets, mixins
+from rest_framework.response import Response
+
+from api.Serialzier.Diet_Calcuation_SZ import Body_info_SZ, Metabolic_Output_SZ
+
+from api.Utils.msg_utils import error_msg
+from metabolic_calculator.models import Metabolic
+
+class Metabolic_Calc_Viewset(viewsets.GenericViewSet, mixins.CreateModelMixin):
+    serializer_class = Body_info_SZ
+
+    def create(self, request):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            ret = serializer.create(serializer.data)
+            return Response(Metabolic_Output_SZ(ret).data, status=status.HTTP_200_OK)
+        return Response(error_msg(serializer=serializer), status=status.HTTP_400_BAD_REQUEST)
