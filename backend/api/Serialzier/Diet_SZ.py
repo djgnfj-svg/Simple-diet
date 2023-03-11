a8d42ec6726b0b34c37aa2bccbdb63bb8bef6b3b
djgnfj-svg <djgnfj8923@naver.com>
2023-03-11 23:29:16 +0900

    style Diet

--- backend/api/Serialzier/Diet_SZ.py
+++ backend/api/Serialzier/Diet_SZ.py
@@ -3,17 +3,19 @@ from rest_framework import serializers
 from meals.Utils.Diet_Manager import Diet_Meal_Calculation_Manager
 
 
-class Diet_Managing_SZ(serializers.Serializer):
+class Diet_SZ(serializers.Serializer):
     total_kcalorie = serializers.IntegerField()
     total_protein = serializers.IntegerField()
     total_fat = serializers.IntegerField()
     total_carbohydrate = serializers.IntegerField()
 
     diet_status = serializers.BooleanField()
-    meal_count = serializers.IntegerField()
-    def create(self, request, validated_data):
+    meal_count = serializers.IntegerField(min_value=1, max_value=4)
+
+    def create(self, validated_data):
         diet_manager = Diet_Meal_Calculation_Manager(validated_data)
         diet_data = diet_manager.get_diet_meal()
+        # todo : 영양소 초기화 로직 클래스 로직으로 처리하기
         diet_data["need_nutrient"] = {}
         for meal in diet_manager._meal_list:
             diet_data["need_nutrient"][meal] = diet_manager.get_meal(meal)
--- backend/api/Views/Diet_Viewset.py
+++ backend/api/Views/Diet_Viewset.py
@@ -3,13 +3,13 @@ from rest_framework.response import Response
 
 from api.Utils.msg_utils import error_msg
 
-from api.Serialzier.Diet_Managing_SZ import Diet_Managing_SZ
+from api.Serialzier.Diet_SZ import Diet_SZ
 
 class Diet_Viewset(viewsets.GenericViewSet, mixins.CreateModelMixin):
-    serializer_class = Diet_Managing_SZ
+    serializer_class = Diet_SZ
     def create(self, request):
         serializer = self.get_serializer(data=request.data)
         if serializer.is_valid():
-            rtn = serializer.create(request, serializer.data)
+            rtn = serializer.create(serializer.data)
             return Response(rtn, status=status.HTTP_201_CREATED)
         return Response(error_msg(serializer=serializer), status=status.HTTP_400_BAD_REQUEST)
