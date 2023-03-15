40c8aaa271056a3dbd426ffd1132698e77d8fc9b
djgnfj-svg <djgnfj8923@naver.com>
2023-03-15 12:38:54 +0900

    drf throtting

--- backend/Simple_diet/settings.py
+++ backend/Simple_diet/settings.py
@@ -178,5 +178,14 @@ else :
 
 REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+
+    'DEFAULT_THROTTLE_CLASSES': [
+        'rest_framework.throttling.AnonRateThrottle',
+        'rest_framework.throttling.UserRateThrottle'
+    ],
+    'DEFAULT_THROTTLE_RATES': {
+        'anon': '100/day',
+        'user': '100/day'
+    }
 }
--- backend/api/Serialzier/Food_Categories_SZ.py
+++ backend/api/Serialzier/Food_Categories_SZ.py
@@ -7,4 +7,4 @@ class Food_Categories_SZ(serializers.ModelSerializer):
     name = serializers.CharField(max_length=50)
     class Meta:
         model = Food_Categories
-        fields = ['name']
+        fields = ['id','name']
