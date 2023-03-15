90af6fd927d77469f0242e1a8c06eb4cc806f573
djgnfj-svg <djgnfj8923@naver.com>
2023-03-15 14:31:07 +0900

    google 애널리틱스

--- backend/api/Serialzier/Metabolic_SZ.py
+++ backend/api/Serialzier/Metabolic_SZ.py
@@ -1,15 +1,7 @@
 from rest_framework import serializers
 
-from metabolic.models import Metabolic
-
-
-
-class Metabolic_SZ(serializers.ModelSerializer):
+class Metabolic_SZ(serializers.Serializer):
     total_kcalorie = serializers.ReadOnlyField()
     total_protein = serializers.ReadOnlyField()
     total_fat = serializers.ReadOnlyField()
     total_carbohydrate = serializers.ReadOnlyField()
-
-    class Meta:
-        model = Metabolic
-        fields = ("total_kcalorie", "total_protein", "total_fat", "total_carbohydrate")
